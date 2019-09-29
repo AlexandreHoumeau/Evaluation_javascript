a16fa4340f402bdd7d93fb6d12b6efa2b2ec0eaf
AlexandreHoumeau <alexandre.houmeau@gmail.com>
2019-09-29 18:58:37 +0200

    i'm almost done

--- Exo3/index.js
+++ Exo3/index.js
@@ -8,8 +8,8 @@
 */
 var MyMorpionXO = function MyMorpionXO(){
 	window.addEventListener('load', function(){
-		this.cases = document.querySelectorAll('.case');
-		
+		this.cases = document.querySelectorAll('.case');	
+
 	});	
 }
 
@@ -50,50 +50,51 @@ MyMorpionXO.prototype.game = function(el){
 */
 MyMorpionXO.prototype.checkWinner = function(){
 
-	while (compt <= 9) {
-		if (cases[0].innerHTML !== '' && cases[0].innerHTML === cases[1].innerHTML && cases[1].innerHTML === cases[2].innerHTML) {
-        alert('The player ' + cases[0].innerHTML + ' won ! yoyo');
-        this.endGame();
-        //window.location.reload();
-        
-	    } else if (cases[3].innerHTML !== '' && cases[3].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[5].innerHTML) {
-	        alert('The player ' + cases[3].innerHTML + ' won !');
-	        window.location.reload();
+	if (cases[0].innerHTML !== '' && cases[0].innerHTML === cases[1].innerHTML && cases[1].innerHTML === cases[2].innerHTML) {
+	    alert('The player ' + cases[0].innerHTML + ' won !');
+	    score.querySelector('.' + cases[0].innerHTML).innerHTML =+ 1;
+	    this.cases.location.reload();
+	        
+    } else if (cases[3].innerHTML !== '' && cases[3].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[5].innerHTML) {
+        alert('The player ' + cases[3].innerHTML + ' won !');
+       	score.querySelector('.' + cases[3].innerHTML).innerHTML =+ 1;
 
-	    } else if (cases[6].innerHTML !== '' && cases[6].innerHTML === cases[7].innerHTML && cases[7].innerHTML === cases[8].innerHTML) {
-	        alert('The player ' + cases[6].innerHTML + ' won !');
-	        window.location.reload();
+        window.location.reload();
 
-	    }
-	    else if (cases[0].innerHTML !== '' && cases[0].innerHTML === cases[3].innerHTML && cases[3].innerHTML === cases[6].innerHTML) {
-	        alert('The player ' + cases[0].innerHTML + ' won !');
-	        window.location.reload();
+    } else if (cases[6].innerHTML !== '' && cases[6].innerHTML === cases[7].innerHTML && cases[7].innerHTML === cases[8].innerHTML) {
+        alert('The player ' + cases[6].innerHTML + ' won !');
+        score.querySelector('.' + cases[6].innerHTML).innerHTML =+ 1;
+        window.location.reload();
+    }
+    else if (cases[0].innerHTML !== '' && cases[0].innerHTML === cases[3].innerHTML && cases[3].innerHTML === cases[6].innerHTML) {
+        alert('The player ' + cases[0].innerHTML + ' won !');
+        score.querySelector('.' + cases[0].innerHTML).innerHTML =+ 1;
+        window.location.reload();
 
-	    } else if (cases[1].innerHTML !== '' && cases[1].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[7].innerHTML) {
-	        alert('The player ' + cases[1].innerHTML + ' won !');
-	        window.location.reload();
+    } else if (cases[1].innerHTML !== '' && cases[1].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[7].innerHTML) {
+        alert('The player ' + cases[1].innerHTML + ' won !');
+        score.querySelector('.' + cases[1].innerHTML).innerHTML =+ 1;
+        window.location.reload();
 
-	    } else if (cases[2].innerHTML !== '' && cases[2].innerHTML === cases[5].innerHTML && cases[5].innerHTML === cases[8].innerHTML) {
-	        alert('The player ' + cases[2].innerHTML + ' won !');
-	        window.location.reload();
-	    }
-	    else if (cases[0].innerHTML !== '' && cases[0].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[8].innerHTML) {
-	        alert('The player ' + cases[0].innerHTML + ' won !');
-	        window.location.reload();
-	    } else if (cases[2].innerHTML !== '' && cases[2].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[6].innerHTML) {
-	        alert('The player ' + cases[2].innerHTML + ' won !');
-	        window.location.reload();
-	    } else if (compt === 9) {
-	    	alert('Match nul');
-	    	window.location.reload();
+    } else if (cases[2].innerHTML !== '' && cases[2].innerHTML === cases[5].innerHTML && cases[5].innerHTML === cases[8].innerHTML) {
+        alert('The player ' + cases[2].innerHTML + ' won !');
+        score.querySelector('.' + cases[2].innerHTML).innerHTML =+ 1;
+        window.location.reload();
+    }
+    else if (cases[0].innerHTML !== '' && cases[0].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[8].innerHTML) {
+        alert('The player ' + cases[0].innerHTML + ' won !');
+        score.querySelector('.' + cases[0].innerHTML).innerHTML =+ 1;
+        window.location.reload();
 
-	    }
-	}
-    
-};
+    } else if (cases[2].innerHTML !== '' && cases[2].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[6].innerHTML) {
+        alert('The player ' + cases[2].innerHTML + ' won !');
+        score.querySelector('.' + cases[2].innerHTML).innerHTML =+ 1;
+        window.location.reload();
 
-MyMorpionXO.prototype.endGame = function(){
-	console.log('Hello');
+    } else if (compt === 9) {
+    	alert('Match nul');
+    	window.location.reload();
+    } 
 };
 
 /**
@@ -104,16 +105,16 @@ MyMorpionXO.prototype.buttonrender = function(){
 
 	for (var i = 0; i < 3; i++){
 		var elDiv = document.createElement('div');
-
+		elDiv.setAttribute('id', 'container');
 		for(var j = 0; j < 3; j++) {
 			var elBut = document.createElement('button');
 			this.game(elBut);
 			
-			//elBut.addEventListener('click', this.checkWinner);
+			elBut.addEventListener('click', this.checkWinner);
 			elDiv.appendChild(elBut);
 
 			
-			elBut.style.background = 'purple';
+			elBut.style.background = 'grey';
 			elBut.style.padding = '70px';
 			elBut.style.borderRadius = '10px';
 			elBut.style.cursor = 'pointer';
@@ -128,10 +129,13 @@ MyMorpionXO.prototype.buttonrender = function(){
 	}	
 
 }
-
 var compt = 0;
 var myMorpionXO = new MyMorpionXO();
 myMorpionXO.render();
 
-
-
+var scores = ['X','O'];
+var score = document.getElementsByTagName('div')[0];
+var scoreX = score.querySelector('.X');
+var score0 = score.querySelector('.O')
+console.log(scoreX.innerHTML);
+console.log(score0.innerHTML);
