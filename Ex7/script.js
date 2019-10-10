7a8145275fd8390cedba2d860315da0ebea24bc3
AlexanrdeHoumeau <alexandre.houmeau@gmail.com>
2019-10-10 20:02:53 +0200

    add normalize() to my string

--- Ex7/script.js
+++ Ex7/script.js
@@ -1,6 +1,6 @@
 
 function checkPalindrome() {
-	var content = document.getElementById('content').value.replace(/[^\w]/gi, '').toLowerCase();
+	var content = document.getElementById('content').value.normalize("NFD").replace(/[^\w]/gi, '').toLowerCase();
 	var palindrome = content.split('').reverse().join('');
 	console.log(palindrome);
 	console.log(content)
