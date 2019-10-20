b6b87edfdcccd2760e069b86da2168daf9deb141
AlexanrdeHoumeau <alexandre.houmeau@gmail.com>
2019-10-20 17:56:07 +0200

    issues adding svg using only js so i add it directly into HTML

--- Exo4/index.js
+++ Exo4/index.js
@@ -3,12 +3,10 @@
  *
 */
 var WorldMap = function WorldMap(){
-	
+	this.country = document.getElementById('country');
 }
-
 WorldMap.prototype.render = function() {
 	this.renderWorldMap();
-	this.interactiveWorldMap();
 };
 
 WorldMap.prototype.renderWorldMap = function() {
@@ -18,48 +16,35 @@ WorldMap.prototype.renderWorldMap = function() {
 	elObj.data = "World_map.svg";
 	elObj.style.width = '100vw';
 	elObj.style.height = '100vh';
-<<<<<<< HEAD
-	elObj.id = 'svg';
-=======
-	elObj.classList.add("world-map");
-	elObj.id = "svg-object"
->>>>>>> 87bdd653ccf9c36b707be580e711dd12af569e65
 
 	elBody.appendChild(elObj);
 
-<<<<<<< HEAD
-	// this.interactiveWorldMap(elObj);
+	this.interactiveWorldMap();
 };
 
 WorldMap.prototype.interactiveWorldMap = function() {
-	var svg = document.getElementById('svg');
-	console.log(svg);
-=======
-	/*this.interactiveWorldMap(elObj);*/
-};
+	window.addEventListener('load', function(){
+		var svg = document.getElementById('svg2');
+		var paths = svg.querySelectorAll('path');
 
-WorldMap.prototype.interactiveWorldMap = function() {
-	
-	
-	/*this.paths.forEach(function(paths) {
-		path.addEventLIstener('mouseenter', function(e) {
-			console.log('salut');
-		})
-	})*/
->>>>>>> 87bdd653ccf9c36b707be580e711dd12af569e65
+		paths.forEach(function(path){
+			path.addEventListener('mouseenter', function(){
+				country.innerHTML = path.id;
+				path.style.fill = 'blue';
+			});
+			path.addEventListener('mouseleave', function(){
+				country.innerHTML = "";
+				path.style.fill = 'black';
+			});
+			path.addEventListener('click', function(){
+				console.log(path.id);
+				path.style.fill = 'red';
+			})
+		});
+	});
 };
 
 var worldMap = new WorldMap();
 worldMap.render();
 
-window.addEventListener('load', function () {
-	var object = document.getElementsByTagName('object').child();
 
-	console.log(object);
-	console.log(object.length);
-	console.log(object[0]);
-
-	
-
-	
-})
