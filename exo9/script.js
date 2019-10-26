524aab6ca8d21ec3f0e0bb48dc13fd8f1eddfea1
AlexanrdeHoumeau <alexandre.houmeau@gmail.com>
2019-10-26 12:42:34 +0200

    add loop to delete .split() .reverse() .join() .normalize() functions

--- exo9/script.js
+++ exo9/script.js
@@ -1,13 +1,37 @@
 
-function checkPalindrome() {
-	var content = document.getElementById('content').value.normalize("NFD").replace(/[^\w]/gi, '').toLowerCase();
-	var palindrome = content.split('').reverse().join('');
-	console.log(palindrome);
-	console.log(content)
+// function checkPalindrome() {
+// 	var content = document.getElementById('content').value.normalize("NFD").replace(/[^\w]/gi, '').toLowerCase();
+// 	var palindrome = content.split('').reverse().join('');
+// 	console.log(palindrome);
+// 	console.log(content)
 
-	if (content === palindrome) {
-		alert('c\'est un palindrome');
+// 	if (content === palindrome) {
+// 		alert('c\'est un palindrome');
+// 	}
+// 	else
+// 		alert('Ce n\'est pas un palindrome');
+// }
+
+function checkPalindrome() {
+	
+	var content = document.getElementById('content').value;
+	console.log(content);
+	var regex = new RegExp(/[^A-Za-z0-9]/g);
+	var palindrome = content.toLowerCase().replace(regex,'');
+
+	for(var i = 0; i < content.length/2; i++){
+		console.log(palindrome[i]);
+
+		if (palindrome[i] !== palindrome[content.length/2 - 1 - i]) {
+			alert('Ce n\'est pas un palindrome !');
+			return false;
+		}
+
+		else{
+			alert('c\'est une palidrome');
+			return true; 
+		}
 	}
-	else
-		alert('Ce n\'est pas un palindrome');
+
+	console.log(palindrome); 
 }
