d59b18320c54c30be5c75ca9c678dfe8fc5ab19c
AlexandreHoumeau <alexandre.houmeau@gmail.com>
2019-10-28 13:41:39 +0100

    have to change the variables on chrome and firefox to work

--- Exo4/index.js
+++ Exo4/index.js
@@ -5,27 +5,36 @@
 var WorldMap = function WorldMap(){
 	this.country = document.getElementById('country');
 }
+
+/**
+ * render
+*/
 WorldMap.prototype.render = function() {
 	this.renderWorldMap();
 	this.interactiveWorldMap();
 };
 
+/**
+ * renderWorldMap
+*/
 WorldMap.prototype.renderWorldMap = function() {
-	// var elBody = document.querySelector('body');
-	// var elObj = document.createElement('object');
+	var elBody = document.querySelector('body');
+	var elObj = document.createElement('object');
 
-	// elObj.data = "World_map.svg";
-	// elObj.style.width = '100vw';
-	// elObj.style.height = '100vh';
+	elObj.data = "World_map.svg";
+	elObj.style.width = '70vw';
+	elObj.style.height = '70vh';
+	elObj.id = 'svg';
 
-	// elBody.appendChild(elObj);
-
-	
+	elBody.appendChild(elObj);	
 };
 
+/**
+ * intercativeWorldMap
+*/
 WorldMap.prototype.interactiveWorldMap = function() {
 	window.addEventListener('load', function(){
-		var svg = document.getElementById('svg2');
+		var svg = document.getElementById('svg').contentDocument; 
 		var paths = svg.querySelectorAll('path');
 
 		paths.forEach(function(path){
