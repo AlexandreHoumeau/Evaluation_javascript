bf88df03913a9c90774a3d10a0e88b26ca12a79f
AlexanrdeHoumeau <alexandre.houmeau@gmail.com>
2019-10-30 18:59:27 +0100

    fixed minor bugs and added comments

--- exo9/script.js
+++ exo9/script.js
@@ -1,37 +1,19 @@
-
-// function checkPalindrome() {
-// 	var content = document.getElementById('content').value.normalize("NFD").replace(/[^\w]/gi, '').toLowerCase();
-// 	var palindrome = content.split('').reverse().join('');
-// 	console.log(palindrome);
-// 	console.log(content)
-
-// 	if (content === palindrome) {
-// 		alert('c\'est un palindrome');
-// 	}
-// 	else
-// 		alert('Ce n\'est pas un palindrome');
-// }
-
 function checkPalindrome() {
 	
 	var content = document.getElementById('content').value;
-	console.log(content);
 	var regex = new RegExp(/[^A-Za-z0-9]/g);
 	var palindrome = content.toLowerCase().replace(regex,'');
+	var len = palindrome.length;
 
-	for(var i = 0; i < content.length/2; i++){
-		console.log(palindrome[i]);
-
-		if (palindrome[i] !== palindrome[content.length/2 - 1 - i]) {
+	for(var i = 0; i < len/2; i++){
+		//Compare the value of the first letter 'palindrome[i]' and the last letter 'palindrome[len - 1 - i]
+		if (palindrome[i] !== palindrome[len- 1 - i]) {
 			alert('Ce n\'est pas un palindrome !');
 			return false;
 		}
-
-		else{
-			alert('c\'est une palidrome');
-			return true; 
-		}
 	}
+	//if the upper condition is not true the function will end the loop and return true
+	alert('C\'est un palindrome !');
+	return true;
 
-	console.log(palindrome); 
 }
